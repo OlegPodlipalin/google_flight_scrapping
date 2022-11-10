5a026f060beae4e456b445fb68f9103d0e5076b4
Oleg Podlipalin <backspace.by@gmail.com>
2022-11-10 23:53:00 +0200

    working variant #1 for milestone 1. some bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # google_flight_scrapping
+### data mining project for ITC
+<p align="center">
+<img src="img/ITC_logo.png" width=150></p>
+
+
+**********MILESTONE 1********** 
+
+
+This program parses oogleflight, a research engine for booking flight. 
+<br>Parsing page, getting important information about flighs according to a specific research. 
+Program made by Oleg Podlipalin and Ruben Adhoute during ITC October 2022 Data Science cohort.
+
+### How to run it
+- Download __*.zip__ file
+- Unzip it at any suitable folder
+- Install all libs from __requirements.txt__
+- Fill in the _CHOOSE_YOUR_OPTION_ parametr (destination)
+- Run __data_extracting.py__
+- Have a cup of cofee
+- That's all
+```
+
+```
+
+
+### Functionality of functions
+Functions are broken down to files. Each file combines logically connected functionality.
+
+| *File*             | *Function*               | *What do and return*                                                                                       |
+|--------------------|--------------------------|------------------------------------------------------------------------------------------------------------|
+| data_extracting.py | main                     | Acts as an orchestrator to program functions. Calls different functions to generate driver and scrap data. |
+| scrapping.py       | scrape_data              | Activate driver and running it on the web page                                                             |
+| driver_action.py   | click_object_class_name  | tell driver to execute the following - click on class object                                               |
+| driver_action.py   | click_object_xpath       | tell driver to execute the following - click on xpath object                                               |
+| driver_action.py   | extend_all               | tell driver to execute the following - extend object                                                       |
+| driver_action.py   | xpath_build              | tell driver to execute the following - build xpath from object                                             |
+| driver_start.py    | start_driver             | initialize the driver given a specific URL                                                                 |
--- data_extracting.py
+++ data_extracting.py
@@ -1,10 +1,13 @@
 import scrapping
 
-Destinations = {'Paris': 'PAR',
-                'Berlin': 'BER',
-                'Amsterdam': 'AMS',
-                'Roma': 'ROM',
-                'Madrid': 'MAD'}
+destinations = {1: 'PAR',
+                2: 'BER',
+                3: 'AMS',
+                4: 'ROM',
+                5: 'MAD'}
+
+CHOOSE_YOUR_OPTION =   # 1 - Paris, 2 - Berlin, 3 - Amsterdam, 4 - Roma, 5 -Madrid
+# CHOOSE_YOUR_OPTION_DAY = not ready yet. the same as with CHOOSE_YOUR_OPTION (destination)
 
 attributes = {'Departure hour': 'dPzsIb AdWm1c y52p7d',
               'Arrival hour': 'SWFQlc AdWm1c y52p7d',
@@ -22,8 +25,7 @@ SCRAPPED_PARAMETERS = {"dept_time": "dPzsIb AdWm1c y52p7d"}
 LI_CLASS_NAME = 'pIav2d'
 flights = dict()
 # source to be imported
-source = 'https://www.google.com/travel/flights?q=' \
-         'Flights%20to%20BER%20from%20TLV%20on%202022-12-20%20through%202022-12-30%20one-way&curr=EUR'
+source = f'https://www.google.com/travel/flights?q=Flights%20to%20{destinations[CHOOSE_YOUR_OPTION]}%20from%20TLV%20on%202022-12-20%20through%202022-12-30%20one-way&curr=EUR'
 
 soup = scrapping.scrape_data(source)
 li_elements = soup.findAll('li', class_=LI_CLASS_NAME)
@@ -38,19 +40,19 @@ for ind, li_element in enumerate(li_elements):
                       for d_time in flight_departure_time]}
 
     flight_arrival_time = li_element.findAll('div', class_=attributes['Arrival hour'])
-    arrival_time = {'Arrival hour': [d_time.text.split()[0] + ' ' + d_time.text.split()[2]
-                    if d_time.text.split() else None
-                    for d_time in flight_departure_time]}
+    arrival_time = {'Arrival hour': [a_time.text.split()[0] + ' ' + a_time.text.split()[2]
+                    if a_time.text.split() else None
+                    for a_time in flight_arrival_time]}
 
     flight_duration = li_element.findAll('div', class_=attributes['Flight duration'])
-    duration = {'Flight duration': [d_time.text.split(':')[1].strip()
-                if d_time.text.split() else None
-                for d_time in flight_departure_time]}
+    duration = {'Flight duration': [f_duration.text.split(':')[1].strip()
+                if f_duration.text.split(':') else None
+                for f_duration in flight_duration]}
 
     flight_price = li_element.findAll('div', class_=attributes['Price'])
-    price = {'Price': [d_time.text
-             if d_time.text.split() else None
-             for d_time in flight_departure_time]}
+    price = {'Price': str({f_price.text
+             if f_price.text else None
+             for f_price in flight_price})}
 
     flights[flight_id_list[ind]] = [departure_time, arrival_time, duration, price]
 
