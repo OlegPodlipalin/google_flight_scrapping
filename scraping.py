28a591e7d5a39dec0159af825cc2641a85474906
Oleg Podlipalin <backspace.by@gmail.com>
2022-11-21 22:22:38 +0200

    all the functionality placed into class Scraper. To use this scraper: 1. initialize; 2. use .run() method; 3. result in .soup field

--- scraping.py
+++ scraping.py
@@ -12,6 +12,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+SHOW_MORE_BUTTON = 'ZVk93d'  # class name of li object. to be imported for each site
+LI_CLASS_NAME = 'pIav2d'  # class name of the first li object . to be imported for each site
+LI_BUTTON_XPATH_TOP = '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/ul/li[1]/div/div[3]/div/div/button'
+LI_BUTTON_XPATH_BOTTOM = '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[5]/ul/li[1]/div/div[3]/div/div/button'
+
+
 class Scraper:
     def __init__(self, url='https://www.google.com', silent_mode=True, delay=5):
         """
@@ -26,7 +32,9 @@ class Scraper:
         self._silent_mode = silent_mode
         self._delay = delay
         self._start_driver()
-        self._waiter = WebDriverWait(self.driver, self._delay)
+        self._waiter = WebDriverWait(self._driver, self._delay)
+        self._elements_to_be_extended = [LI_BUTTON_XPATH_TOP, LI_BUTTON_XPATH_BOTTOM]
+        self._destination = 'Berlin'
 
     def _start_driver(self):
         options = Options()
@@ -35,49 +43,52 @@ class Scraper:
         if not self._silent_mode:
             driver.set_window_size(1920, 1080)
         driver.get(self._url)
-        self.driver = driver
+        self._driver = driver
         print("the driver is started")
 
-    def click_object_by_class_name(self, address):
+    def _click_object_by_class_name(self, address):
         self._waiter.until(EC.presence_of_element_located((By.CLASS_NAME, address)))
         self._waiter.until(EC.element_to_be_clickable((By.CLASS_NAME, address))).click()
 
-    def click_object_by_xpath(self, address):
+    def _click_object_by_xpath(self, address):
         self._waiter.until(EC.presence_of_element_located((By.XPATH, address)))
         self._waiter.until(EC.element_to_be_clickable((By.XPATH, address))).click()
 
-    def move_to_element_by_class_name(self, address):
+    def _move_to_element_by_class_name(self, address):
         element = self._waiter.until(EC.presence_of_element_located((By.CLASS_NAME, address)))
-        actions = ActionChains(self.driver)  # move up to the very first element
+        actions = ActionChains(self._driver)  # move up to the very first element
         actions.move_to_element(element).perform()
 
-    def move_to_element_by_xpath(self, address):
-        element = self._waiter.until(EC.presence_of_element_located((By.XPATH, address)))
-        actions = ActionChains(self.driver)  # move up to the very first element
-        actions.move_to_element(element).perform()
+    # def _move_to_element_by_xpath(self, address):
+    #     element = self._waiter.until(EC.presence_of_element_located((By.XPATH, address)))
+    #     actions = ActionChains(self.driver)  # move up to the very first element
+    #     actions.move_to_element(element).perform()
 
-    def souping(self):
+    def _souping(self):
         sleep(1)
-        return BeautifulSoup(self.driver.page_source, "lxml")
+        self.soup = BeautifulSoup(self._driver.page_source, "lxml")
+        self._amount = len(self.soup.findAll('li', class_=LI_CLASS_NAME))
 
+    def _address_builder(self, xpath, li_ind):
+        ind = xpath[::-1].index('l')
+        return xpath[:-ind + 2] + str(li_ind) + xpath[-ind + 3:]
 
-def address_builder(xpath, li_ind):
-    ind = xpath[::-1].index('l')
-    return xpath[:-ind + 2] + str(li_ind) + xpath[-ind + 3:]
+    def run(self):
+        self._click_object_by_class_name(SHOW_MORE_BUTTON)
+        self._move_to_element_by_class_name(LI_CLASS_NAME)
+        self._souping()
 
+        bar = tqdm(desc=f'{self._destination} destination scraping:', total=self._amount,
+                   bar_format="{desc:<10}{percentage:3.0f}%|{bar:50}{r_bar}")
+        for base_address in self._elements_to_be_extended:
+            for n in range(1, self._amount + 1):
+                try:
+                    self._click_object_by_xpath(self._address_builder(base_address, n))
+                    bar.update(1)
+                except TimeoutException:
+                    break
 
-def extend_all(driver, base_addresses, amount, destination):
-    bar = tqdm(desc=f'{destination} destination scraping:', total=amount,
-               bar_format="{desc:<10}{percentage:3.0f}%|{bar:50}{r_bar}")
-    for base_address in base_addresses:
-        for n in range(1, amount + 1):
-            try:
-                address = address_builder(base_address, n)
-                # driver.move_to_element_by_xpath(address)
-                driver.click_object_by_xpath(address)
-                bar.update(1)
-            except TimeoutException:
-                break
+        self._souping()
 
 
 def main():
