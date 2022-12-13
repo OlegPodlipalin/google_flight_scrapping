5975cee81383fcfd28c34e0e3a4faf48bb9b7c28
Oleg Podlipalin <backspace.by@gmail.com>
2022-12-13 22:16:38 +0200

    parser.py. Added: logging functionality; status bar with direction and date. Slight code refactoring

--- webpage_tools/parser.py
+++ webpage_tools/parser.py
@@ -1,40 +1,44 @@
 import re
+import logging
+from tqdm import tqdm
 from datetime import datetime, date, time
 from libraries.get_from_library import get_data
-
-ATTRIB = {'Departure': ['div', 'dPzsIb AdWm1c y52p7d'],
-          'Arrival': ['div', 'SWFQlc AdWm1c y52p7d'],
-          'Flight duration': ['div', 'CQYfx y52p7d'],
-          'Flight number': ['div', 'MX5RWe sSHqwe y52p7d'],
-          # 'Connection time': ['div', 'tvtJdb eoY5cb y52p7d'],
-          'Price': ['div', ['YMlIz FpEdX', 'YMlIz FpEdX jLMuyc']],
-          'CO2 emission': ['span', 'gI4d6d'],
-          'Facilities': ['ul', 'elO9Ce sSHqwe JNp8ff']}
-
-# to be imported
-LI_CLASS_NAME = 'pIav2d'
 
 
 class GoogleFlightsParser:
     def __init__(self, soup):
-        self.flights = dict()
+        logging.info('GoogleFlightsParser instance creation...')
+        self._data = get_data('scraping_parsing')
         self._month_dict = get_data('months')
         self._today = date.today()
-        self._li_elements = soup.findAll('li', class_=LI_CLASS_NAME)
+        self._li_elements = soup[0].findAll('li', class_=self._data['li_class_name'])
+        self._webpage = soup[1]
+        self._destination = soup[2]
+
+        self.trips = dict()
+        self._price = None
+        self._current_trip_id = None
+        self._facilities = []
+        self._flight_time = []
+        self._co2_emission = []
+        self._flight_number = []
+        self._flight_airport = []
+        self._flight_duration = []
+        logging.info('GoogleFlightsParser instance created')
+
         self.run()
 
-    def _get_flight_id(self, element_data):
+    def _get_trip_id(self, element_data):
+        logging.debug(f'Getting "Trip id" for {self._destination}')
         element_data_string = str(element_data).split()
         # li_string = [str(element).split() for element in self._li_elements]
-        self._current_flight_id = element_data_string[4].split('"')[1]  # indexes were checked manually on the site
+        self._current_trip_id = element_data_string[4].split('"')[1]  # indexes were checked manually on the site
 
     def _get_departure_arrival(self, element_data, attribute_name):
-        # self._get_element(element_data, attribute_name)
-        self._current_flight_data = element_data.findAll(ATTRIB[attribute_name][0], class_=ATTRIB[attribute_name][1])
-        self._flight_time = []
-        self._flight_airport = []
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
 
-        for flight_elements in self._current_flight_data:
+        for flight_elements in current_trip_data:
             if not flight_elements:
                 self._flight_time.append(None)
                 self._flight_airport.append(None)
@@ -62,73 +66,74 @@ class GoogleFlightsParser:
             self._flight_time.append(datetime(year, month, day, hours, minutes))
             self._flight_airport.append(airport)
 
-    def _time_components_checker(self, elements):
-        if elements[1][0] != 'h':
-            self._current_flight_hours = 0
-            self._current_flight_minutes = int(elements[0])
-        elif len(elements) < 3:
-            self._current_flight_hours = int(elements[0])
-            self._current_flight_minutes = 0
-        else:
-            self._current_flight_hours = int(elements[0])
-            self._current_flight_minutes = int(elements[2])
-
     def _get_flight_duration(self, element_data, attribute_name):
-        # self._get_element(element_data, attribute_name)
-        self._current_flight_data = element_data.findAll(ATTRIB[attribute_name][0], class_=ATTRIB[attribute_name][1])
-        self._flight_duration = []
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
 
-        for flight_elements in self._current_flight_data:
+        for flight_elements in current_trip_data:
             if not flight_elements:
                 self._flight_duration.append(None)
                 continue
             duration_elements = flight_elements.text.split(':')[1].split()
-            self._time_components_checker(duration_elements)
-            self._flight_duration.append(time(self._current_flight_hours,
-                                              self._current_flight_minutes))  # what formate of time is better to store in database?
+
+            if duration_elements[1][0] != 'h':
+                current_flight_hours = 0
+                current_flight_minutes = int(duration_elements[0])
+            elif len(duration_elements) < 3:
+                current_flight_hours = int(duration_elements[0])
+                current_flight_minutes = 0
+            else:
+                current_flight_hours = int(duration_elements[0])
+                current_flight_minutes = int(duration_elements[2])
+
+            self._flight_duration.append(time(current_flight_hours, current_flight_minutes))
 
     def _get_flight_number(self, element_data, attribute_name):
-        self._current_flight_data = element_data.findAll(ATTRIB[attribute_name][0], class_=ATTRIB[attribute_name][1])
-        self._flight_number = []
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
 
-        for flight_elements in self._current_flight_data:
+        for flight_elements in current_trip_data:
             if not flight_elements:
                 self._flight_number.append(None)
                 continue
             self._flight_number.append(flight_elements.get_text("##").split('##')[-1])
 
     def _get_price(self, element_data, attribute_name):
-        # self._get_element(element_data, attribute_name)
-        self._current_flight_data = element_data.findAll(ATTRIB[attribute_name][0], class_=ATTRIB[attribute_name][1])
-        self._price = None
-        for flight_elements in self._current_flight_data:
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
+        for flight_elements in current_trip_data:
             if flight_elements:
                 self._price = int(re.sub(r'\D+', '', flight_elements.text))
                 break
 
     def _get_co2_emission(self, element_data, attribute_name):
-        # self._get_element(element_data, attribute_name)
-        self._current_flight_data = element_data.findAll(ATTRIB[attribute_name][0], class_=ATTRIB[attribute_name][1])
-        self._co2_emission = []
-        for flight_elements in self._current_flight_data:
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
+
+        for flight_elements in current_trip_data:
             if not flight_elements:
                 self._co2_emission.append(None)
                 continue
             self._co2_emission.append(flight_elements.text.split()[-2])
 
-    def _get_facilities(self, element_data, attrib_name):
-        self._current_flight_data = element_data.findAll(ATTRIB[attrib_name][0], class_=ATTRIB[attrib_name][1])
-        self._facilities = []
-        for flight_elements in self._current_flight_data:
-            self._facilities_list = flight_elements.get_text('##').split('##')
-            for ind, facility in enumerate(self._facilities_list):
+    def _get_facilities(self, element_data, attribute_name):
+        logging.debug(f'Getting "{attribute_name}" for {self._destination}')
+        current_trip_data = element_data.findAll(self._data[attribute_name][0], class_=self._data[attribute_name][1])
+
+        for flight_elements in current_trip_data:
+            facilities_list = flight_elements.get_text('##').split('##')
+            for ind, facility in enumerate(facilities_list):
                 if facility[:6] == 'Carbon':
-                    self._facilities.append(self._facilities_list[:ind])
+                    self._facilities.append(facilities_list[:ind])
                     break
 
     def run(self):
-        for li_element in self._li_elements:
-            self._get_flight_id(li_element)
+        logging.info(f'Start parsing {self._webpage}')
+        number_elements = len(self._li_elements)
+        bar = tqdm(desc=f'{self._destination} parsing', total=number_elements)
+        for n, li_element in enumerate(self._li_elements):
+            logging.debug(f'Parsing element {n}/{number_elements} from {self._webpage}')
+            self._get_trip_id(li_element)
 
             self._get_departure_arrival(li_element, 'Departure')
             departure_info = {'Departure_hour': self._flight_time} | {'Departure_airport': self._flight_airport}
@@ -151,8 +156,19 @@ class GoogleFlightsParser:
             self._get_facilities(li_element, 'Facilities')
             facilities_info = {'Facilities': self._facilities}
 
-            self.flights[self._current_flight_id] = departure_info | arrival_info | duration_info | number_info | \
-                                                    price_info | co2_emission_info | facilities_info
+            logging.debug(f'All parameters for element {n}/{number_elements} from {self._webpage} received')
+            self.trips[self._current_trip_id] = departure_info | arrival_info | duration_info | number_info | \
+                                                price_info | co2_emission_info | facilities_info
 
-# for ind, flight in enumerate(flights.items()):
-#     print(ind, '\t', flight)
+            self._price = None
+            self._current_trip_id = None
+            self._facilities = []
+            self._flight_time = []
+            self._co2_emission = []
+            self._flight_number = []
+            self._flight_airport = []
+            self._flight_duration = []
+
+            bar.update(1)
+        self.trips['destination'] = self._destination
+        logging.info(f'All elements form {self._webpage} parsed')
