49408136a0fc32121a01c868d8479fbc2e7da6a0
Oleg Podlipalin <backspace.by@gmail.com>
2022-12-13 22:20:17 +0200

    get_from_library.py. Added: logging functionality; wrapped into try-except (raises FileNotFoundError).

--- libraries/get_from_library.py
+++ libraries/get_from_library.py
@@ -1,3 +1,4 @@
+import logging
 import json
 import os
 
@@ -6,5 +7,13 @@ def get_data(name):
     suffix = '.json'
     dir_path = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(dir_path, os.path.join('', name + suffix))
-    with open(path, 'r') as file:
-        return json.load(file)
+    logging.info(f'Getting data from {name} library')
+    try:
+        logging.info(f'Opening {path}')
+        with open(path, 'r') as file:
+            result = json.load(file)
+            logging.info(f'Data from "{name}" library extracted')
+            return result
+    except FileNotFoundError:
+        logging.error(f'File not found: {path}')
+        raise FileNotFoundError(f'File not found: {path}')
